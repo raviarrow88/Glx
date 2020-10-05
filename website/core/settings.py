b4be5314dabe3dfe29a1a506c1068ac2ed30240a
ravi <ravitejap@sendfast.in>
2020-10-05 20:18:35 +0530

    removed '

--- website/core/settings.py
+++ website/core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-'
+
 ]
 
 
